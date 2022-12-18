d9f3180524aa5796cf67d12ec9a73e08cdb57d6c
UkokuGemini <ukoku@live.cn>
2022-12-18 18:14:12 +0800

    test tools

--- js/Live2d.js
+++ js/Live2d.js
@@ -35,7 +35,7 @@ if (screen.width >= 768) {
 			waifuPath: live2d_path + "waifu-tips.json",
 			//apiPath: "https://live2d.fghrsh.net/api/",
 			cdnPath: "https://fastly.jsdelivr.net/gh/fghrsh/live2d_api/"
-			tools: ["hitokoto", "photo", "quit"]
+			tools: ["hitokoto","photo","quit"]
 		});
 	});
 }
