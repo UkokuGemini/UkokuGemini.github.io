c2ca7bff360fd0b348f07d2291804116f64509e5
UkokuGemini <ukoku@live.cn>
2022-12-20 00:16:08 +0800

    douban

--- js/douban.js
+++ js/douban.js
@@ -4,6 +4,11 @@ function doubane ()
     .then(response => response.json())
     .then(data => {
       if (true) {
-        document.getElementById('douban').innerHTML = data.hitokoto
+          var out = "";
+          var i;
+          for(i = 0; i < data.length; i++) {
+            out += '<a href="' + data[i].thumbnail + '">' + data[i].title + '</a><br>';
+          }
+        document.getElementById('douban').innerHTML = out
       }})
 }doubane ()
