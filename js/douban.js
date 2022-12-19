f4a34b801840534c5b79564b2efbc3ac4066d698
UkokuGemini <ukoku@live.cn>
2022-12-19 19:57:17 +0800

    .

--- js/douban.js
+++ js/douban.js
@@ -1,5 +1,6 @@
-function douban () {
+function douban () 
+{
     fetch('https://v1.hitokoto.cn')
       .then(response => response.json())
       .then(data => {
-        if (true) {alert('  ·谨言出自:  ' + data.from +"/" + data.from_who)})}
+        if (true) {alert('  ·谨言出自:  ' + data.from +"/" + data.from_who)}}}
