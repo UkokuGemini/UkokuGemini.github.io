d162559412ec2d2daaf48287a250e205f58e1878
UkokuGemini <ukoku@live.cn>
2022-12-20 12:49:42 +0800

    .trtdb

--- js/douban.js
+++ js/douban.js
@@ -8,7 +8,7 @@ function doubane ()
           var outD = "";
           var i;
           for(i = 0; i < dataD.length; i++) {
-            outD += '<div height="100"><table frame=void><tr><td><img height="100" src="https://images.weserv.nl/?url=' + dataD[i].item.thumbnail.substring(7) + '"></td><td><a href="https://movie.douban.com/subject/' + dataD[i].item.douban_id + '/">' + dataD[i].item.title + '</a><br><p><font size="5">' + dataD[i].item.actor + '</font></p></td></tr></table></div><hr>';
+            outD += '<div height="100"><table frame=void><tr><td><img height="100" src="https://images.weserv.nl/?url=' + dataD[i].item.thumbnail.substring(7) + '"></td><td><a href="https://movie.douban.com/subject/' + dataD[i].item.douban_id + '/"><b>' + dataD[i].item.title + '</b></a><br><p><font size="5">' + dataD[i].item.actor + '</font></p></td></tr></table></div><hr>';
           }
         document.getElementById('douban').innerHTML = outD
       }})
