f826688056684c2149a24d65d6da769b73e1343d
UkokuGemini <ukoku@live.cn>
2022-12-20 12:22:27 +0800

    .

--- js/douban.js
+++ js/douban.js
@@ -8,7 +8,7 @@ function doubane ()
           var outD = "";
           var i;
           for(i = 0; i < dataD.length; i++) {
-            outD += '<div height="100"><img height="75" src="' + dataD[i].item.thumbnail + '"><br><p>' + dataD[i].item.title + '</p></div><br>';
+            outD += '<div height="100"><img height="75" src="' + dataD[i].item.thumbnail + '"><br><a href="https://movie.douban.com/subject/' + dataD[i].item.douban_id + '/">' + dataD[i].item.title + '</a><br><a>' + dataD[i].item.actor + '</a></div><hr>';
           }
         document.getElementById('douban').innerHTML = outD
       }})
