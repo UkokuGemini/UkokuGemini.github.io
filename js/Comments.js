ffbe27d14facd95baa077d4e292a16b376f9a5c4
UkokuGemini <ukoku@live.cn>
2023-03-05 02:10:04 +0800

    Version /1.1.3

---
+++ js/Comments.js
@@ -0,0 +1,20 @@
+window.CUSDIS_LOCALE = {
+  powered_by: 'UkokuGemini®️  Cusdis™️                      >評論需要待管理員審核通過後顯示',
+  post_comment: '📨發送',
+  loading: '🕐加載中',
+
+  // comment
+  email: '📧電郵地址 (可選)',
+  nickname: '🤪暱稱',
+  reply_placeholder: '📝回覆內容…',
+  reply_btn: '📨回覆',
+  sending: '📨發送中…',
+
+  // reply
+  mod_badge: '管理員',
+  content_is_required: '內容不能為空白',
+  nickname_is_required: '暱稱不能為空白',
+
+  comment_has_been_sent:
+    '📨評論已發送，待管理員審核通過後將會顯示',
+}
