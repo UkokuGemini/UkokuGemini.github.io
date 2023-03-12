e45516dec8cd0b1667946ed1fc86deeaf5e08f77
UkokuGemini <ukoku@live.cn>
2023-03-12 18:17:09 +0800

    取消蛛网效果在鼠标上的粘滞

--- js/NetEffect.js
+++ js/NetEffect.js
@@ -9,7 +9,7 @@
     
     function t() {
     var t = e("script"), o = t.length, i = t[o - 1];
-    return {l: o, z: n(i, "zIndex", -1), o: n(i, "opacity", .5), c: n(i, "color", "0,0,0"), n: n(i, "count", 99)}
+    return {l: o, z: n(i, "zIndex", -1), o: n(i, "opacity", .5), c: n(i, "color", "0,0,0"), n: n(i, "count", 120)}
     }
     
     function o() {
@@ -28,11 +28,8 @@
     x = window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.oRequestAnimationFrame || window.msRequestAnimationFrame || function (n) {
     window.setTimeout(n, 1e3 / 45)
     }, w = Math.random, y = {x: null, y: null, max: 2e4};
-    m.id = l, m.style.cssText = "position:fixed;top:0;left:0;z-index:" + d.z + ";opacity:" + d.o, e("body")[0].appendChild(m), o(), window.onresize = o, window.onmousemove = function (n) {
-    n = n || window.event, y.x = n.clientX, y.y = n.clientY
-    }, window.onmouseout = function () {
-    y.x = null, y.y = null
-    };
+    m.id = l, m.style.cssText = "position:fixed;top:0;left:0;z-index:" + d.z + ";opacity:" + d.o, e("body")[0].appendChild(m), o(), window.onresize = o//, window.onmousemove = function (n) {n = n || window.event, y.x = n.clientX, y.y = n.clientY}, window.onmouseout = function () {y.x = null, y.y = null}
+    ;
     for (var s = [], f = 0; d.n > f; f++) {
     var h = w() * a, g = w() * c, v = 2 * w() - 1, p = 2 * w() - 1;
     s.push({x: h, y: g, xa: v, ya: p, max: 6e3})
